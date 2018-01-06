b372db618084b8bee53d463412706f3caa954be2
zaizizaizai <zaizizaizai@163.com>
2018-01-06 16:42:15 +0800

    获取数据

--- 温度监控/web/model/lists.js
+++ 温度监控/web/model/lists.js
@@ -4,6 +4,19 @@ module.exports = function(){
     //读取data中数据
     var data = JSON.parse(fs.readFileSync(__dirname + '/../data/data.json'));
     var lists = {
-        
+        getAllLists: getAllLists,
+        getLastList: getLastList
+    };
+
+    //获取所有list
+    function getAllLists(){
+        return data.lists;
     }
-}
+
+    //获取最新的list
+    function getLastList(){
+        return data.lists[data.lists.length - 1];
+    }
+
+    return lists;
+};
